f51f331e42302f6c41189380756cc22f2b684660
Frakos21 <121487054+Frakos21@users.noreply.github.com>
2023-03-15 17:10:19 +0700

    Menambahkan class kode buah
    menambahkan class kode buah

---
+++ modul4_1302210063/PosisiKarakterGame.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4_1302210063
+{
+    internal class PosisiKarakterGame
+    {
+        public enum stateKarakter {Jongkok, Berdiri, Tengkurap, Terbang }
+        public enum Trigger { TombolW, TombolS, TombolX}
+
+        public class game
+        {
+            public stateKarakter currentState = stateKarakter.Jongkok;
+            public class Transition
+            {
+                public stateKarakter stateAwal;
+                public stateKarakter stateAkhir;
+                public Trigger trigger;
+
+                public Transition(stateKarakter stateAwal, stateKarakter stateAkhir, Trigger trigger)
+                {
+                    this.stateAwal = stateAwal;
+                    this.stateAkhir = stateAkhir;
+                    this.trigger = trigger;
+                }
+            }
+            Transition[] transisi =
+            {
+                new Transition(stateKarakter.Berdiri, stateKarakter.Terbang, Trigger.TombolW),
+                new Transition(stateKarakter.Terbang, stateKarakter.Berdiri, Trigger.TombolS),
+                new Transition(stateKarakter.Berdiri, stateKarakter.Jongkok, Trigger.TombolS),
+                new Transition(stateKarakter.Jongkok, stateKarakter.Berdiri, Trigger.TombolW),
+                new Transition(stateKarakter.Terbang, stateKarakter.Jongkok, Trigger.TombolX),
+                new Transition(stateKarakter.Jongkok, stateKarakter.Tengkurap, Trigger.TombolS),
+                new Transition(stateKarakter.Tengkurap, stateKarakter.Jongkok, Trigger.TombolW)
+
+            };
+            public stateKarakter nextState(stateKarakter stateAwal, Trigger trigger) {
+                stateKarakter finalState = stateAwal;
+                for(int i = 0;i<transisi.Length;i++)
+                {
+                    Transition temp = transisi[i];
+                    if(stateAwal == temp.stateAwal && trigger == temp.trigger)
+                    {
+                        finalState = temp.stateAkhir;
+                    }
+                }
+                return finalState;
+            }
+            public void activateTrigger(Trigger trigger)
+            {
+                
+            }
+        }
+    }
+}
---
+++ modul4_1302210063/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Security.Cryptography.X509Certificates;
+
+
+
+public class KodeBuah{
+    public enum Buah
+    {
+        apel,aprikot, alpukat, pisang, paprika, blackberry, ceri, kelapa, jagung
+    } 
+    public static String getKodeBuah(Buah input)
+    {
+        String[] KodeBuah = { "A00", "B00", "C00", "D00", "E00", "F00", "G00", "H00", "I00", "J00" };
+        return KodeBuah[(int)input];
+    }
+    public static void Main(String[] args)
+    {
+        Buah inputBuah = Buah.alpukat;
+        String result = getKodeBuah(inputBuah);
+        Console.WriteLine("Nama Buah: "+inputBuah+"\nKode Buah: "+result);
+    }
+}
